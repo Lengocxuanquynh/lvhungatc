68f9ce98816422a17f6c14790f9461272a63a222
Lengocxuanquynh <quynhle.040606@gmail.com>
2026-08-02 16:52:06 +0700

    fix

--- src/app/actions/settings.ts
+++ src/app/actions/settings.ts
@@ -116,8 +116,7 @@ export async function updateSiteSettingsAction(formData: FormData) {
       }
     });
 
-    revalidatePath("/");
-    revalidatePath("/dashboard/settings");
+    revalidatePath("/", "layout");
     
     return { success: true };
   } catch (error: any) {
